35f8a5cb01dc4197cac335e436260a3226292d6f
ROREYO <104793028+ROREYO@users.noreply.github.com>
2023-07-18 22:10:51 +0300

    Add files via upload

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,61 @@
+import project01 from './../img/projects/01.jpg';
+import projectBig01 from './../img/projects/01-big.jpg';
+
+import project02 from './../img/projects/02.jpg';
+import projectBig02 from './../img/projects/02-big.jpg';
+
+import project03 from './../img/projects/03.jpg';
+import projectBig03 from './../img/projects/03-big.jpg';
+
+import project04 from './../img/projects/04.jpg';
+import projectBig04 from './../img/projects/04-big.jpg';
+
+import project05 from './../img/projects/05.jpg';
+import projectBig05 from './../img/projects/05-big.jpg';
+
+import project06 from './../img/projects/06.jpg';
+import projectBig06 from './../img/projects/06-big.jpg';
+
+const projects = [
+  {
+    title: 'Gaming streaming portal',
+    skills: 'React, Node.js, MongoDB',
+    img: project01,
+    imgBig: projectBig01,
+    gitHubLink: 'https://github.com',
+  },
+  {
+    title: 'Video service',
+    skills: 'React, PHP, MySql',
+    img: project02,
+    imgBig: projectBig02,
+    gitHubLink: 'https://github.com',
+  },
+  {
+    title: 'Video portal',
+    skills: 'React, Node.js, MongoDB',
+    img: project03,
+    imgBig: projectBig03,
+    gitHubLink: 'https://github.com',
+  },
+  {
+    title: 'Dating app',
+    skills: 'React Native',
+    img: project04,
+    imgBig: projectBig04,
+  },
+  {
+    title: 'Landing',
+    skills: 'HTML, CSS, JS',
+    img: project05,
+    imgBig: projectBig05,
+  },
+  {
+    title: 'Gaming community',
+    skills: 'React, PHP, MySql',
+    img: project06,
+    imgBig: projectBig06,
+  },
+];
+
+export { projects };
